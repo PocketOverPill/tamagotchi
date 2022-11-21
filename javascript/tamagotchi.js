bb35882b792e06238a0ab3035992c8d4563f0784
Bruno Ribeiro <bruno.marshall22@gmail.com>
2022-11-21 20:17:43 -0300

    Ajustes classes

--- javascript/tamagotchi.js
+++ javascript/tamagotchi.js
@@ -1,2 +1,9 @@
-var tamagotchi = {
+class Tamagotchi{
+    constructor(alt, lag, fome, alegria, exist){
+    this.alt = alt;
+    this.lag = lag;
+    this.fome = fome;
+    this.alegria = alegria;
+    this.exist = exist;
+    }
 }
